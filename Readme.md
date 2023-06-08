6531ceb8e1928304101d8c454230c80f49883c1d
Leyojin Thomas <leyojinthomas36@gmail.com>
2023-06-08 20:52:37 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+#selfout_server   
+This is a simple back-end code written in Node Js for a Self Checking Out Application.
+
+#Description   
+Selfout is a Mobile Application build using Flutter , Node Js and Mongodb.
+Costumers can checkout products they want to purchase from a shop by scanning their bar codes by themselves.
+
+#installation   
+git clone https://github.com/karthik-cj/selfout_server.git  
+cd selfout_server/  
+configure the database url in index.js file  
+npm install  
+npm run dev || npm start  
+
+#Thank You
---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+const DB = "mongodb+srv://karthik:karthik123@cluster0.vie2q.mongodb.net/test";
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(authRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
